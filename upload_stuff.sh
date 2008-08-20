487f6fceec2093c4de730a4937ab5c815012a5e0
Andrew Straw <strawman@astraw.com>
2008-08-20 14:34:59 +0200

    fix file for release

--- upload_stuff.sh
+++ upload_stuff.sh
@@ -5,14 +5,14 @@
 # generate index.html from README.txt
 rst2html --stylesheet=../../default.css --link-stylesheet -g -d -s README.txt index.html
 
-VERSION=0.1
+VERSION=0.2
 
 # generate the .zip file
 rm -rf tmp
 mkdir tmp
 bzr export tmp/drosophila_eye_map-$VERSION
 cd tmp
-zip -9 -r drosophila_eye_map-$VERSION.zip drosophila_eye_map-0.1
+zip -9 -r drosophila_eye_map-$VERSION.zip drosophila_eye_map-$VERSION
 cd ..
 
 # upload the files

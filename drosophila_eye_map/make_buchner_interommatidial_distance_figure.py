49e8a82ba2f0da50533094a65adc4521d9f7a457
Andrew Straw <strawman@astraw.com>
2008-10-18 20:27:25 -0700

    convert to standard python main() idiom

--- drosophila_eye_map/make_buchner_interommatidial_distance_figure.py
+++ drosophila_eye_map/make_buchner_interommatidial_distance_figure.py
@@ -34,27 +34,6 @@ import drosophila_eye_map.precomputed_buchner71 as precomputed_buchner_1971
 from drosophila_eye_map.util import get_mean_interommatidial_distance, xyz2lonlat
 from mpl_toolkits.basemap import Basemap # basemap > 0.9.9.1
 
-rdirs = precomputed_buchner_1971.receptor_dirs
-rdir_slicer = precomputed_buchner_1971.receptor_dir_slicer
-triangles = precomputed_buchner_1971.triangles
-
-dists = np.array(get_mean_interommatidial_distance( rdirs[rdir_slicer['left']], triangles ))
-R2D = 180.0/np.pi
-dists = dists*R2D
-
-lon_lats = [xyz2lonlat(*rdir) for rdir in rdirs[rdir_slicer['left']]]
-
-stere = Basemap(projection='stere',
-                resolution=None,
-                lat_ts = 0.0,
-                lat_0 = 0,
-                lon_0 = 90,
-                llcrnrlon = -45,
-                urcrnrlon = -135,
-                llcrnrlat= -30,
-                urcrnrlat = 30,
-                )
-
 def do_projection( proj, lon_lats, dists, xres = 120, yres = 100 ):
     xys = np.array([proj( lon, lat ) for (lon,lat) in lon_lats ])
 
@@ -74,146 +53,169 @@ def do_projection( proj, lon_lats, dists, xres = 120, yres = 100 ):
     Z = np.ma.masked_array(vals,mask=np.isnan(vals))
     return x,y,X,Y,Z
 
-x,y,X,Y,Z = do_projection(stere,lon_lats,dists)
+def main():
+    rdirs = precomputed_buchner_1971.receptor_dirs
+    rdir_slicer = precomputed_buchner_1971.receptor_dir_slicer
+    triangles = precomputed_buchner_1971.triangles
 
-import matplotlib
-rcParams = matplotlib.rcParams
-rcParams['font.size'] = 10
-rcParams['font.family'] = 'serif'
-#rcParams['font.serif'] = 'Times'
-#rcParams['font.sans-serif'] = 'Arial'
-if 0:
-    from matplotlib import verbose
-    verbose.level = 'debug-annoying'
+    dists = np.array(get_mean_interommatidial_distance( rdirs[rdir_slicer['left']], triangles ))
+    R2D = 180.0/np.pi
+    dists = dists*R2D
 
-import matplotlib.pyplot as plt
-import matplotlib.cm
+    lon_lats = [xyz2lonlat(*rdir) for rdir in rdirs[rdir_slicer['left']]]
 
-# pcolor figure -- stereographic projection
-fig3 = plt.figure(3)
-ax = plt.subplot(1,1,1)
-plt.pcolor(Y,X,Z,shading='flat',cmap=matplotlib.cm.jet_r)
+    stere = Basemap(projection='stere',
+                    resolution=None,
+                    lat_ts = 0.0,
+                    lat_0 = 0,
+                    lon_0 = 90,
+                    llcrnrlon = -45,
+                    urcrnrlon = -135,
+                    llcrnrlat= -30,
+                    urcrnrlat = 30,
+                    )
 
-ax.plot(x,y,'wo',ms=4.0)
+    x,y,X,Y,Z = do_projection(stere,lon_lats,dists)
 
-ax.text( 0.5,0.99, 'dorsal',
-         horizontalalignment='center',
-         verticalalignment='top',
-         transform=ax.transAxes)
-ax.text( 0.01,0.5, 'anterior',
-         horizontalalignment='left',
-         transform=ax.transAxes)
-ax.text( 0.99,0.5, 'posterior',
-         horizontalalignment='right',
-         transform=ax.transAxes)
+    import matplotlib
+    rcParams = matplotlib.rcParams
+    rcParams['font.size'] = 10
+    rcParams['font.family'] = 'serif'
+    #rcParams['font.serif'] = 'Times'
+    #rcParams['font.sans-serif'] = 'Arial'
+    if 0:
+        from matplotlib import verbose
+        verbose.level = 'debug-annoying'
 
-# draw parallels and meridians.
-delat = 20.
-circles = np.arange(0.,90.,delat).tolist()+\
-          np.arange(-delat,-90,-delat).tolist()
-#biw.stere.drawparallels(circles,ax=ax)
-stere.drawparallels(circles,ax=ax)
+    import matplotlib.pyplot as plt
+    import matplotlib.cm
 
-delon = 45.
-meridians = np.arange(-180,180,delon)
-stere.drawmeridians(meridians,ax=ax)
-cbar = plt.colorbar()
-cbar.ax.set_ylabel('mean inter-ommatidial distance (deg)')
+    # pcolor figure -- stereographic projection
+    fig3 = plt.figure(3)
+    ax = plt.subplot(1,1,1)
+    plt.pcolor(Y,X,Z,shading='flat',cmap=matplotlib.cm.jet_r)
 
-save_fname = 'interommatidial_distance.png'
-print 'saving',save_fname
-fig3.savefig(save_fname)
-print 'OK'
+    ax.plot(x,y,'wo',ms=4.0)
 
+    ax.text( 0.5,0.99, 'dorsal',
+             horizontalalignment='center',
+             verticalalignment='top',
+             transform=ax.transAxes)
+    ax.text( 0.01,0.5, 'anterior',
+             horizontalalignment='left',
+             transform=ax.transAxes)
+    ax.text( 0.99,0.5, 'posterior',
+             horizontalalignment='right',
+             transform=ax.transAxes)
 
+    # draw parallels and meridians.
+    delat = 20.
+    circles = np.arange(0.,90.,delat).tolist()+\
+              np.arange(-delat,-90,-delat).tolist()
+    #biw.stere.drawparallels(circles,ax=ax)
+    stere.drawparallels(circles,ax=ax)
 
-# contour figure -- stereographic projection
-fig1 = plt.figure(1)
-ax = plt.subplot(1,1,1)
-CS = plt.contour(Y,X,Z,
-                   #levels=np.linspace(4,16,25),
-                   levels=np.linspace(4,16,13),
-                   colors='k',
-                   )
-plt.clabel(CS,
-           fmt='%.1f',
-           colors='k')
+    delon = 45.
+    meridians = np.arange(-180,180,delon)
+    stere.drawmeridians(meridians,ax=ax)
+    cbar = plt.colorbar()
+    cbar.ax.set_ylabel('mean inter-ommatidial distance (deg)')
 
-ax.plot(x,y,'wo',ms=4.0)
-
-ax.text( 0.5,0.99, 'dorsal',
-         horizontalalignment='center',
-         verticalalignment='top',
-         transform=ax.transAxes)
-ax.text( 0.01,0.5, 'anterior',
-         horizontalalignment='left',
-         transform=ax.transAxes)
-ax.text( 0.99,0.5, 'posterior',
-         horizontalalignment='right',
-         transform=ax.transAxes)
-
-# draw parallels and meridians.
-delat = 20.
-circles = np.arange(0.,90.,delat).tolist()+\
-          np.arange(-delat,-90,-delat).tolist()
-stere.drawparallels(circles,ax=ax)
-
-delon = 45.
-meridians = np.arange(-180,180,delon)
-stere.drawmeridians(meridians,
-                    ax=ax)
+    save_fname = 'interommatidial_distance.png'
+    print 'saving',save_fname
+    fig3.savefig(save_fname)
+    print 'OK'
 
 
-# contour figure -- orthographic projection
-fig2 = plt.figure(2)
 
-# Match projection of
-# http://jeb.biologists.org/cgi/content/full/209/21/4339/FIG1
-ortho = Basemap(projection='ortho',
-                lat_0=10,
-                lon_0=20,
-                resolution=None,
-                )
+    # contour figure -- stereographic projection
+    fig1 = plt.figure(1)
+    ax = plt.subplot(1,1,1)
+    CS = plt.contour(Y,X,Z,
+                       #levels=np.linspace(4,16,25),
+                       levels=np.linspace(4,16,13),
+                       colors='k',
+                       )
+    plt.clabel(CS,
+               fmt='%.1f',
+               colors='k')
 
-x,y,X,Y,Z = do_projection(ortho,lon_lats,dists,xres=500,yres=500)
+    ax.plot(x,y,'wo',ms=4.0)
 
-ax = plt.subplot(1,1,1)
-CS = plt.contour(Y,X,Z,
-                 #levels=np.linspace(4,16,25),
-                 levels=np.arange(4,16.5,1.0),
-                 colors='k',
-                 linewidths=2.0,
-                 )
-plt.clabel(CS,fmt='%.1f',colors='k')
+    ax.text( 0.5,0.99, 'dorsal',
+             horizontalalignment='center',
+             verticalalignment='top',
+             transform=ax.transAxes)
+    ax.text( 0.01,0.5, 'anterior',
+             horizontalalignment='left',
+             transform=ax.transAxes)
+    ax.text( 0.99,0.5, 'posterior',
+             horizontalalignment='right',
+             transform=ax.transAxes)
 
-#ax.plot(x,y,'wo',ms=4.0)
-ax.set_aspect('equal')
+    # draw parallels and meridians.
+    delat = 20.
+    circles = np.arange(0.,90.,delat).tolist()+\
+              np.arange(-delat,-90,-delat).tolist()
+    stere.drawparallels(circles,ax=ax)
 
-# draw parallels and meridians.
-delat = 10.
-circles = np.arange(0.,90.,delat).tolist()+\
-          np.arange(-delat,-90,-delat).tolist()
-circles = [c for c in circles if c!= 0.0]
-ortho.drawparallels(circles,ax=ax)
-ortho.drawparallels([0.0],linestyle='-',dashes=[],ax=ax)
-
-delon = 10.
-meridians = np.arange(-180,180,delon).tolist()
-meridians = [m for m in meridians if m!=0.0]
-ortho.drawmeridians(meridians,ax=ax)
-ortho.drawmeridians([0.0],linestyle='-',dashes=[],ax=ax)
-ortho.drawmapboundary(ax=ax)
-
-# Text p. 12 of Heisenberg, M. and Wolf, R., (1984) implies this was a
-# female.
-#plt.title(unichr(2640)+' average inter-ommatidial angle (degrees)')
-plt.title('female average inter-ommatidial angle (degrees)')
-
-save_fname = 'interommatidial_distance_ortho.png'
-print 'saving',save_fname
-fig2.savefig(save_fname)
-print 'OK'
+    delon = 45.
+    meridians = np.arange(-180,180,delon)
+    stere.drawmeridians(meridians,
+                        ax=ax)
 
 
-plt.show()
+    # contour figure -- orthographic projection
+    fig2 = plt.figure(2)
 
+    # Match projection of
+    # http://jeb.biologists.org/cgi/content/full/209/21/4339/FIG1
+    ortho = Basemap(projection='ortho',
+                    lat_0=10,
+                    lon_0=20,
+                    resolution=None,
+                    )
+
+    x,y,X,Y,Z = do_projection(ortho,lon_lats,dists,xres=500,yres=500)
+
+    ax = plt.subplot(1,1,1)
+    CS = plt.contour(Y,X,Z,
+                     #levels=np.linspace(4,16,25),
+                     levels=np.arange(4,16.5,1.0),
+                     colors='k',
+                     linewidths=2.0,
+                     )
+    plt.clabel(CS,fmt='%.1f',colors='k')
+
+    #ax.plot(x,y,'wo',ms=4.0)
+    ax.set_aspect('equal')
+
+    # draw parallels and meridians.
+    delat = 10.
+    circles = np.arange(0.,90.,delat).tolist()+\
+              np.arange(-delat,-90,-delat).tolist()
+    circles = [c for c in circles if c!= 0.0]
+    ortho.drawparallels(circles,ax=ax)
+    ortho.drawparallels([0.0],linestyle='-',dashes=[],ax=ax)
+
+    delon = 10.
+    meridians = np.arange(-180,180,delon).tolist()
+    meridians = [m for m in meridians if m!=0.0]
+    ortho.drawmeridians(meridians,ax=ax)
+    ortho.drawmeridians([0.0],linestyle='-',dashes=[],ax=ax)
+    ortho.drawmapboundary(ax=ax)
+
+    # Text p. 12 of Heisenberg, M. and Wolf, R., (1984) implies this was a
+    # female.
+    #plt.title(unichr(2640)+' average inter-ommatidial angle (degrees)')
+    plt.title('female average inter-ommatidial angle (degrees)')
+
+    save_fname = 'interommatidial_distance_ortho.png'
+    print 'saving',save_fname
+    fig2.savefig(save_fname)
+    print 'OK'
+
+    plt.show()
+
+if __name__ == '__main__':
+    main()

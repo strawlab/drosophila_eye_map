c2449673760ede26dea3fd6c6202f776a6e5adee
Andrew Straw <strawman@astraw.com>
2008-10-20 09:07:15 -0700

    release script also creates and uploads tarballs

--- upload_stuff.sh
+++ upload_stuff.sh
@@ -4,9 +4,12 @@
 
 echo "Andrew: copy the README.txt file into ~/src/astraw-private-repo/work-website/input-code/drosophila_eye_map/README.txt"
 
+# Make sure this corresponds to the value in setup.py and README.txt
 VERSION=0.4
 
 git archive --format=zip -v --prefix=drosophila_eye_map-$VERSION/ release-$VERSION > ../drosophila_eye_map-$VERSION.zip
+git archive --format=tar -v --prefix=drosophila_eye_map-$VERSION/ release-$VERSION > ../drosophila_eye_map-$VERSION.tar
+gzip ../drosophila_eye_map-$VERSION.tar
 
 # upload the files
-rsync -avzP ../drosophila_eye_map-$VERSION.zip code.astraw.com:/var/websites/code.astraw.com/drosophila_eye_map/download/
+rsync -avzP ../drosophila_eye_map-$VERSION.zip ../drosophila_eye_map-$VERSION.tar.gz code.astraw.com:/var/websites/code.astraw.com/drosophila_eye_map/download/

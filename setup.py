d4ad30817399bec8f457b7ba0906715f6f87006f
Andrew Straw <strawman@astraw.com>
2017-07-27 10:05:52 +0200

    cleanup setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2005-2008, California Institute of Technology
+# Copyright (c) 2017, Albert-Ludwigs-Universität Freiburg
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -30,24 +31,30 @@
 # This file specifies how to install the drosophila_eye_map package
 # for use by other Python programs and libraries.
 
-from setuptools import setup, find_packages
 import os
+from setuptools import setup, find_packages
 
-if not os.path.exists(os.path.join('drosophila_eye_map','precomputed_buchner71.py')):
-    raise ValueError('precomputed_buchner71.py is missing, hint: "cd drosophila_eye_map && python precompute_buchner71_optics.py"')
+FNAMES = [os.path.join(*args) for args in [
+    ('drosophila_eye_map', 'receptor_directions_buchner71.csv'),
+    ('drosophila_eye_map', 'precomputed_buchner71.py'),
+    ('drosophila_eye_map', 'receptor_weight_matrix_64_buchner71.mat'),
+]]
+
+for fname in FNAMES:
+    if not os.path.exists(fname):
+        raise ValueError('%s is missing, hint: '
+                         '"cd drosophila_eye_map && python precompute_buchner71_optics.py"' % fname)
 
 setup(name='drosophila_eye_map',
       description='eye map of Drosophila melanogaster',
       author='Andrew Straw',
       author_email='strawman@astraw.com',
       version='0.4-git', # keep in sync with upload_stuff.sh and README.txt
-      packages = find_packages(),
-      package_data = {'drosophila_eye_map':['receptor_weight_matrix_64_buchner71.mat',
-                                            #'precomputed_buchner71.py', # automatically installed since it's a Python file
-                                            ]},
-      entry_points = {
-       'console_scripts': [
-         'drosophila_eye_map_inspect_weightmap = drosophila_eye_map.inspect_weightmap:main',
-         ],
-       }
-      )
+      packages=find_packages(),
+      package_data={'drosophila_eye_map':['receptor_weight_matrix_64_buchner71.mat',]},
+      entry_points={
+          'console_scripts': [
+              'drosophila_eye_map_inspect_weightmap = drosophila_eye_map.inspect_weightmap:main',
+          ],
+      }
+     )
